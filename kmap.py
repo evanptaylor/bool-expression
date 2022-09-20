432947a1644aef96e2cfbe9a9c3ad13f37781a8d
evanptaylor <36122439+evanptaylor@users.noreply.github.com>
2022-09-20 19:41:00 -0400

    testing functionality

--- kmap.py
+++ kmap.py
@@ -1,15 +1,45 @@
 # 2x2 Kmap solver
 
-# fill out entire map with 0 or 1 based on input
+class Kmap(object):
+    
+    def __init__(self):
+        self.kmap = [0]*4
+        self.size = 4
 
-def fill(indexes):
-    kmap = [0]*4
-    for i in range(0, 4):
-        if i in indexes:
-            kmap[i] = 1
-    return kmap
+    def get_true(self):
+        true_vals = 0
+        for i in self.kmap:
+            if i==1:
+                true_vals+=1
+        return true_vals
+    # fill out entire map with 0 or 1 based on input
+    def fill(self, function):
+        self.kmap = [0]*self.size
+        for i in range(0, self.size):
+            if i in function:
+                self.kmap[i] = 1
+        return self.kmap
+
+    def split(self): 
+        arr = []
+        sub_arr = []
+        arr.append(self.kmap)
+        for i in range(0,len(self.kmap)-1):
+            sub_arr.append(self.kmap[i])
+            sub_arr.append(self.kmap[i+1])
+            arr.append(sub_arr)
+            sub_arr.clear()
+        return arr
+
+
         
 
-mykmap = [0,1]
 
-print(fill(mykmap))
+
+myfunction = [0,1]
+kmap = Kmap()
+print(kmap.fill(myfunction))
+print(kmap.get_true())
+print(kmap.split())
+
+
