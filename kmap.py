14e0f750c9f3be8259497269b6a8387a787cfb32
evanptaylor <36122439+evanptaylor@users.noreply.github.com>
2022-09-21 19:44:57 -0400

    setting up size 2 and 4 implicants

--- kmap.py
+++ kmap.py
@@ -1,8 +1,9 @@
 # Kmap solver using Quine-McClusky Algorithm
 
-# dec_to_bin converts each int in an array to their 4-bit decimal values
 num_vars = 4
-minterms = [0,1,4,6,8,9,14,15]
+minterms = [4,8,9,10,11,12,14,15]
+minterms_ind = [4,8,9,10,11,12,14,15]
+# dec_to_bin converts each int in an array to their 4-bit decimal values
 
 def dec_to_bin(list):
     for i in range(0, len(list)):
@@ -31,12 +32,12 @@ def off_by_one(str1, str2):
     off_by = 0
     temp_str = ""
     for i in range(0, len(str1)):
-        if str1[i] == "-" or str2[i] == "-":
+        if str1[i]=="*" or str2[i]=="*":
             return str1
         if str1[i] != str2[i]:
             off_by+=1
     if off_by != 1:
-        return str1
+        return str1+"*"
     else:
         for i in range(0, len(str1)):
             if str1[i] != str2[i]:
@@ -45,21 +46,51 @@ def off_by_one(str1, str2):
                 temp_str+=str1[i]
         return temp_str
 
-# replace_nums replaces the alike digits in the dictionary
+# replace_nums replaces the alike digits 
 def replace_nums(arr):
+    replaced_arr = []
+    replaced_indexes = []
+    temp_str = ""
     for i in range(0, len(arr)):
         for j in range(i+1,len(arr)):
-            arr[i] = off_by_one(arr[i],arr[j])
-                
+            temp_str = off_by_one(arr[i],arr[j])
+            if "-" in temp_str:
+                replaced_arr.append(temp_str)
+                replaced_indexes.append((minterms_ind[i],minterms_ind[j]))
+    return replaced_indexes, replaced_arr
+
+def size4_imp(arr):
+    size4 = []
+    size4_idx = []
+    count = 0
+    for i in range(0,len(arr)):
+        if arr[i].count("-") == 2:
+            size4.append(arr[i])
+            size4_idx.append(size2_implicants_idx)
+    return size4, size4_idx
 
 
 
-print(dec_to_bin(minterms))
+
+dec_to_bin(minterms)
 
 #print(group_ones(minterms))
 
-print(minterms)
-print(replace_nums(minterms))
-print(minterms)
+size2_implicants = replace_nums(minterms)
+
+size2_implicants_idx = size2_implicants[0]
+size2_implicants_val = size2_implicants[1]
+minterms_ind = size2_implicants[0]
+
+
+#print(size2_implicants_idx)
+#print(size2_implicants_val)
+
+size4_implicants = replace_nums(size2_implicants_val)
+print(size4_implicants[1])
+print(size4_imp(size4_implicants[1]))
+
+#print(minterms)
+#print(minterms_ind)
 
 
